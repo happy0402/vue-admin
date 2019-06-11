4431e93bf40022d076abca18535d983b31387076
happy0402 <420196346@qq.com>
2019-06-11 15:56:24 +0800

    提交文件

--- src/introduction/views/index.vue
+++ src/introduction/views/index.vue
@@ -16,10 +16,14 @@
         data(){
             return {
                 code: `//github 代码提交
-                cd d:/ZKX/VueWorkspace/vue-admin
-                git add ./src/introduction
-                git add ./src/library
-                `
+    cd d:/ZKX/VueWorkspace/vue-admin
+    git add src/introduction
+    git add src/library
+    git stash -u -k
+    git status //查看状态
+    git commit -m '提交文件'
+    git pull  //拉去合并
+    git push  //推到远端仓库`
             }
         }
     }
